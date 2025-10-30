06bb063e61ec392a21709c6499a714752cce55fe
UOdrones <154944718+UOdrones@users.noreply.github.com>
2025-10-30 18:40:32 -0500

    Add Tailwind CSS configuration fileAdd tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        oailRed: '#c00',
+        graphite: '#1a1a1a'
+      }
+    },
+  },
+  plugins: [],
+}
