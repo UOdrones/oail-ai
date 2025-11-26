28dd691e5eadd6170dfe8a7b54706eb2e516005d
UOdrones <154944718+UOdrones@users.noreply.github.com>
2025-11-26 07:32:47 -0600

    Improve favicon visibility

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import faviconUrl from './three/OAIL3.png'
+import faviconUrl from './three/favicon.svg'
 
 const existingFavicon = document.querySelector("link[rel='icon']")
 const favicon = existingFavicon || document.createElement('link')
 favicon.rel = 'icon'
+favicon.type = 'image/svg+xml'
+favicon.sizes = 'any'
 favicon.href = faviconUrl
 
 if (!existingFavicon) {
