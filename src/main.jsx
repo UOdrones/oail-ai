2b6180e3eecba1b1262bf15d3b8735490a0db22b
UOdrones <154944718+UOdrones@users.noreply.github.com>
2025-11-26 07:24:59 -0600

    Use bundled logo for favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import faviconUrl from './three/OAIL3.png'
+
+const existingFavicon = document.querySelector("link[rel='icon']")
+const favicon = existingFavicon || document.createElement('link')
+favicon.rel = 'icon'
+favicon.href = faviconUrl
+
+if (!existingFavicon) {
+  document.head.appendChild(favicon)
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
